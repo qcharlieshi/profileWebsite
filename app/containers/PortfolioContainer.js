b71a4a354053d7a7c846b057daddee71f4104b23
Charlie Shi <CharlieS@live.com>
2017-05-18 15:17:16 -0400

    Added routes for medium and portfolio, changed portfolio view

--- app/containers/PortfolioContainer.js
+++ app/containers/PortfolioContainer.js
@@ -6,33 +6,46 @@ import React, {Component} from 'react';
 import { Card, CardImg, CardText, CardBlock,
   CardTitle, CardSubtitle, Button, CardColumns, CardHeader, CardLink } from 'reactstrap';
 
-import PortfolioItemComponent from '../components/PortfolioItemComponent';
+import PortfolioCard from '../components/PortfolioCard';
 
 
 export default class PortfolioContainer extends Component {
-
   render() {
+    const portfolioHeaderImageStyle = {
+      backgroundImage: 'url(\'../images/portfolioHeader.png\')',
+      height: '200px',
+      zIndex: '1000',
+      width: '100%',
+      boxShadow: '0px 15px 5px rgba(0, 0, 0, 0.5)'
+    }
+
+    const cardColumnStyle = {
+      width: '100%',
+      padding: '5%',
+      paddingTop: '25px',
+      backgroundColor: '#0B3C5D'
+    }
+
     return (
         <div>
-          <div className="row">
-            <div style={{backgroundImage: 'url(\'../images/portfolioHeader.png\')', height: '200px', zIndex: '-1000'}}></div>
+          <div className="row" style={{height: '200px', width: '101%'}}>
+            <div style={portfolioHeaderImageStyle}></div>
           </div>
 
-          {/*<div className="portfolioPage">*/}
+          <div className="card-columns" style={cardColumnStyle}>
+            <PortfolioCard />
+            <PortfolioCard />
+            <PortfolioCard />
+            <PortfolioCard />
+            <PortfolioCard />
+            <PortfolioCard />
+            <PortfolioCard />
 
-          <div className="card" style={{width: '20rem'}}>
-            <img className="card-img-top" src="..." alt="Card image cap" />
-              <div className="card-block">
-                <h4 className="card-title">Card title</h4>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-              </div>
+
           </div>
-
-
-
-          {/*</div>*/}
         </div>
     )
+
+
   }
 }
